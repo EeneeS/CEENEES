8ea57086c8ca03a59c54cfe6db277afd7694587d
eenees <soeteniels1803@gmail.com>
2024-06-06 17:41:04 +0200

    make it possible to pass files to the program to read

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *read_file();
+char *read_file(const char *filename);
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s <filename>\n", argv[0]);
+    return 1;
+  }
 
-  char *source_code = read_file();
+  char *source_code = read_file(argv[1]);
 
   if (source_code != NULL) {
     printf("%s", source_code);
@@ -15,16 +19,15 @@ int main() {
   return 0;
 }
 
-char *read_file() {
-
+char *read_file(const char *filename) {
   FILE *fptr;
   char *source_code;
   long file_size;
 
-  fptr = fopen("source_code.ce", "r");
+  fptr = fopen(filename, "r");
 
   if (fptr == NULL) {
-    perror("Error opening file.");
+    perror("Error opening file");
     return NULL;
   }
 
@@ -34,12 +37,18 @@ char *read_file() {
 
   source_code = malloc(file_size + 1);
   if (source_code == NULL) {
-    perror("Error allocation memory");
+    perror("Error allocating memory");
     fclose(fptr);
     return NULL;
   }
 
-  fread(source_code, 1, file_size, fptr);
+  if (fread(source_code, 1, file_size, fptr) != file_size) {
+    perror("Error reading file");
+    free(source_code);
+    fclose(fptr);
+    return NULL;
+  }
+
   source_code[file_size] = '\0';
   fclose(fptr);
   return source_code;
