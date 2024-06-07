5fb7fecdd8b2d5c34df871117f0bcd9cffa54bd1
eenees <soeteniels1803@gmail.com>
2024-06-07 11:45:52 +0200

    add more operations

--- lexer.c
+++ lexer.c
@@ -10,8 +10,14 @@ typedef enum {
   TOKEN_KEYWORD,
   TOKEN_NUMBER,
   TOKEN_PLUS,
+  TOKEN_MULTIPLY,
+  TOKEN_INCREMENT,
+  TOKEN_DECREMENT,
+  TOKEN_EXPONENTIATION,
   TOKEN_MINUS,
   TOKEN_SLASH,
+  TOKEN_EQUALITY,
+  TOKEN_ASSIGNMENT,
   TOKEN_EOF,
 } TokenType;
 
@@ -32,6 +38,7 @@ typedef struct {
 void lexer_init(Lexer *lexer, char *input);
 void lexer_advance(Lexer *lexer);
 void lexer_skip_whitespace(Lexer *lexer);
+char *read_operator(Lexer *lexer);
 char *read_word(Lexer *lexer);
 char *read_number(Lexer *lexer);
 Token lexer_next_token(Lexer *lexer);
@@ -41,7 +48,7 @@ void free_token(Token *token);
 void lexer_init(Lexer *lexer, char *input) {
   lexer->source_code = input;
   lexer->position = 0;
-  lexer->read_position = 0;
+  lexer->read_position = 1;
   lexer->current_char = input[0];
 }
 
@@ -72,19 +79,47 @@ Token lexer_next_token(Lexer *lexer) {
     token.value = "EOF";
     break;
   case '+':
-    token.type = TOKEN_PLUS;
-    token.value = "+";
-    lexer_advance(lexer);
+    if (lexer->source_code[lexer->read_position] == '+') {
+      lexer_set_token(&token, TOKEN_INCREMENT, "++");
+      lexer_advance(lexer);
+      lexer_advance(lexer);
+    } else {
+      lexer_set_token(&token, TOKEN_PLUS, "+");
+      lexer_advance(lexer);
+    }
     break;
   case '-':
-    token.type = TOKEN_MINUS;
-    token.value = "-";
-    lexer_advance(lexer);
+    if (lexer->source_code[lexer->read_position] == '-') {
+      lexer_set_token(&token, TOKEN_DECREMENT, "--");
+      lexer_advance(lexer);
+      lexer_advance(lexer);
+    } else {
+      lexer_set_token(&token, TOKEN_MINUS, "-");
+      lexer_advance(lexer);
+    }
+    break;
+  case '*':
+    if (lexer->source_code[lexer->read_position] == '*') {
+      lexer_set_token(&token, TOKEN_EXPONENTIATION, "**");
+      lexer_advance(lexer);
+      lexer_advance(lexer);
+    } else {
+      lexer_set_token(&token, TOKEN_MULTIPLY, "*");
+      lexer_advance(lexer);
+    }
     break;
   case '/':
-    token.type = TOKEN_SLASH;
-    token.value = "/";
+    lexer_set_token(&token, TOKEN_SLASH, "/");
     lexer_advance(lexer);
+  case '=':
+    if (lexer->source_code[lexer->read_position] == '=') {
+      lexer_set_token(&token, TOKEN_EQUALITY, "==");
+      lexer_advance(lexer);
+      lexer_advance(lexer);
+    } else {
+      lexer_set_token(&token, TOKEN_ASSIGNMENT, "=");
+      lexer_advance(lexer);
+    }
     break;
   default:
     if (isalpha(lexer->current_char)) {
@@ -101,6 +136,27 @@ void lexer_set_token(Token *token, TokenType type, char *value) {
   token->type = type;
   token->value = value;
 }
+
+// char *read_operator(Lexer *lexer) {
+//
+//   size_t start_position = lexer->position;
+//   size_t length = 1;
+//
+//   for (int i = 0; i < strlen(operators); i++) {
+//     if (lexer->current_char == operators[i]) {
+//       length++;
+//       lexer_advance(lexer);
+//       break;
+//     }
+//   }
+//
+//   // TODO: check if a valid operator
+//
+//   char *operator=(char *) malloc(length + 1);
+//   strncpy(operator, lexer->source_code + start_position, length);
+//   operator[length] = '\0';
+//   return operator;
+// }
 
 char *read_word(Lexer *lexer) {
   size_t start_position = lexer->position;
